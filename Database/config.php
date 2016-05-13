a66b9466c05232a356d1edb9d00e9656bd630f10
jon-wilson <jwilson@wilsonfamilydevelopers.com>
2016-05-12 21:47:43 -0400

    Adding database script to repository

---
+++ Database/config.php
@@ -0,0 +1,29 @@
+<?php
+    $server_name = "localhost";
+	//keep it as local host only.
+    $db_name = "bhargavc_police";
+	//database name.
+    $user_name = "siddhartha";
+	//db username.
+    $db_pswd = "123456";
+	//db user password and make sure that db has mapped to that user.
+    $link = mysqli_connect($server_name, $user_name, $db_pswd, $db_name);
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+    if (!function_exists("dateForm2DB")) {
+        function dateForm2DB($frm_date)
+        {
+            $frm_date = explode("/", $frm_date);
+            if (!empty($frm_date[0]) && !empty($frm_date[1]) && !empty($frm_date[2])) {
+                return $frm_date[2] . "-" . $frm_date[1] . "-" . $frm_date[0];
+            } else {
+                return "";
+            }
+        }
+    }
+
+
