ef583903bf76edba8fc827fd4c0138aa387b102e
jon-wilson <jwilson@wilsonfamilydevelopers.com>
2016-12-13 09:04:09 -0500

    combined system with demo version

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@ app.config(function($routeProvider, $locationProvider) {
 		   templateUrl: 'views/dashboard.html',
 		   controller: 'DashboardCtrl'
 		}).
+		when('/admin', {
+		   templateUrl: 'views/admin.html',
+		   controller: 'AdminCtrl'
+		}).
 		otherwise({
 			redirectTo: '/login'
 		});		
@@ -22,82 +26,251 @@ app.config(function($routeProvider, $locationProvider) {
 /***
 *	Dashboard Controller
 ***/
-app.controller('DashboardCtrl', function($scope, $location, locationHandler, userSession) {
+app.controller('AdminCtrl', function($scope, locationHandler, userSession) {	
 
-	$scope.isActiveView = function(id){	
-	}	// End function	
-	
-	Date.prototype.addDays = function(days){
-		var dat = new Date(this.valueOf());
-		dat.setDate(dat.getDate() + days);
-		return dat;
-	}
+}	// End Admin Controller
 
-	var date = new Date();
-	var day = date.getDate();
-	var monthIndex = date.getMonth();
-	var year = date.getFullYear();
+app.controller('DashboardCtrl', function($scope, $location, locationHandler, userSession) {	
+	// Constant variables
+	var LOWERYEARLIMIT = new Date().getFullYear() - 2;
+	var UPPERYEARLIMIT = new Date().getFullYear() + 3;
 	
-	var daysOftheMonth = [];
-	for( var i = 1; i <= new Date(year, monthIndex + 1, 0).getDate(); i++ )
-		daysOftheMonth.push(i);	
-	$scope.getDays = {
-		availableOptions : daysOftheMonth,
-		selectedOption : day
+	// Initialize local functions	
+	function LoadViews(){
+		var _viewNames = [ "month", "week", "day" ];
+		return { availableOptions : _viewNames, selectedOption : _viewNames[0] };
+	}	// End function LoadViews
+	
+	function LoadDays(year, month){		
+		var _days = [];
+		var _date = new Date(year, month + 1, 0);
+		for( var _i = 1; _i <= _date.getDate(); _i++ )
+			_days.push(_i);	
+		
+		return { availableOptions : _days, selectedOption : new Date().getDate() };	
+	}	// End function LoadDays
+	
+	Date.prototype.getWeekOfMonth = function() {
+		var month = this.getMonth()
+			, year = this.getFullYear()
+			, firstWeekday = new Date(year, month, 1).getDay()
+			, lastDateOfMonth = new Date(year, month + 1, 0).getDate()
+			, offsetDate = this.getDate() + firstWeekday - 1
+			, index = 1 // start index at 0 or 1, your choice
+			, weeksInMonth = index + Math.ceil((lastDateOfMonth + firstWeekday - 7) / 7)
+			, week = index + Math.floor(offsetDate / 7)
+		;
+		return week;
 	};
 	
-	var monthNames = [ 
-		{ id: '1', name: "January", value: 0 },
-		{ id: '2', name: "February", value: 1 },
-		{ id: '3', name: "March", value: 2 },
-		{ id: '4', name: "April", value: 3 },
-		{ id: '5', name: "May", value: 4 },
-		{ id: '6', name: "June", value: 5 },
-		{ id: '7', name: "July", value: 6 },
-		{ id: '8', name: "August", value: 7 },
-		{ id: '9', name: "September", value: 8 },
-		{ id: '10', name: "October", value: 9 },
-		{ id: '11', name: "November", value: 10 },
-		{ id: '12', name: "December", value: 11 } 
-	];
-	$scope.getMonths = { 
-		availableOptions : monthNames,
-		selectedOption : monthNames[monthIndex]
-	}
+	function LoadWeeks(){
+		var _weeks = [ 				
+			{ id: '1', name: "Week: 1", value: 1 },
+			{ id: '2', name: "Week: 2", value: 2 },
+			{ id: '3', name: "Week: 3", value: 3 },
+			{ id: '4', name: "Week: 4", value: 4 },
+			{ id: '5', name: "Week: 5", value: 5 }
+		];		
+		return { availableOptions : _weeks, selectedOption : _weeks[new Date().getWeekOfMonth() - 1]};	
+	}	// End function LoadWeeks
 
-	var years = [];
-	for( var i = year - 2; i <= year + 3; i++ )
-		years.push(i);	
-	$scope.getYears =  {
-		availableOptions : years,
-		selectedOption : year
-	};	
+	function LoadMonths(){
+		var _monthNames = [ 	
+			{ id: '1', name: "January", value: 0 },
+			{ id: '2', name: "February", value: 1 },
+			{ id: '3', name: "March", value: 2 },
+			{ id: '4', name: "April", value: 3 },
+			{ id: '5', name: "May", value: 4 },
+			{ id: '6', name: "June", value: 5 },
+			{ id: '7', name: "July", value: 6 },
+			{ id: '8', name: "August", value: 7 },
+			{ id: '9', name: "September", value: 8 },
+			{ id: '10', name: "October", value: 9 },
+			{ id: '11', name: "November", value: 10 },
+			{ id: '12', name: "December", value: 11 } 
+		];
+		var _monthIndex = new Date().getMonth();
+		return { availableOptions : _monthNames, selectedOption : _monthNames[_monthIndex] };
+	}	// End function LoadMonths
 	
-	$scope.getMonthData = function(){	
-		var monthData = [];
-		var monthIndex = $scope.getMonths.selectedOption.value;
-		var year = $scope.getYears.selectedOption;
-		var firstDayofCalendar = new Date(year, monthIndex, 1);
+	function LoadYears(){
+		var _years = [];
+		for( var i = LOWERYEARLIMIT; i <= UPPERYEARLIMIT; i++ )
+			_years.push(i);	
 		
-		//What date is the first sunday of the month?
-		if(firstDayofCalendar.getDay() > 0)
-			firstDayofCalendar.setDate(1 - firstDayofCalendar.getDay());
-		else
-			firstDayofCalendar.setDate(-6);
-		
-		for(var i=0; i < (7*6); i++){
-			if( monthIndex === firstDayofCalendar.addDays(i).getMonth() )
-				if( date.toDateString() === firstDayofCalendar.addDays(i).toDateString() )
-					monthData.push({id : i, day : firstDayofCalendar.addDays(i).getDate(), isToday : true, thisMonth : true});
-				else
-					monthData.push({id : i, day : firstDayofCalendar.addDays(i).getDate(), isToday : false, thisMonth : true})
-			else
-				monthData.push({id : i, day : firstDayofCalendar.addDays(i).getDate(), isToday : false, thisMonth : false});
-		} 		
-		$scope.monthData = monthData;	
+		return { availableOptions : _years, selectedOption : new Date().getFullYear() };	
+	}	// End function LoadYears	
+	
+	Date.prototype.addDays = function(days){
+		var date = new Date(this.valueOf());
+		date.setDate(date.getDate() + days);
+		return date;
 	}
 	
-	$scope.getMonthData();
+	function GetWeekData(){
+		var _calendarData = [];
+		var _weekIndex = $scope.getWeeks.selectedOption.value;
+		var _monthIndex = $scope.getMonths.selectedOption.value;
+		var _year = $scope.getYears.selectedOption;		
+		var _firstDayofCalendar = new Date(_year, _monthIndex, 1);
+		
+		//What date is the first sunday of the week?	
+		if(_firstDayofCalendar.getDay() > 0)
+			_firstDayofCalendar.setDate(1 - _firstDayofCalendar.getDay() + (7 * ( _weekIndex - 1 )));
+		else
+			_firstDayofCalendar.setDate(-6  + (7 * ( _weekIndex - 1 )));		
+			
+		for(var _i=0; _i < 7; _i++){
+			if( _monthIndex === _firstDayofCalendar.addDays(_i).getMonth() )
+				if( new Date().toDateString() === _firstDayofCalendar.addDays(_i).toDateString() )
+					_calendarData.push({id : _i, day : _firstDayofCalendar.addDays(_i).getDate(), isToday : true, thisMonth : true});
+				else
+					_calendarData.push({id : _i, day : _firstDayofCalendar.addDays(_i).getDate(), isToday : false, thisMonth : true})
+			else
+				_calendarData.push({id : _i, day : _firstDayofCalendar.addDays(_i).getDate(), isToday : false, thisMonth : false});
+		} 
+		
+		return _calendarData;	
+	}	// End function GetWeekData
+	
+	function GetMonthData(){
+		var _calendarData = [];
+		var _monthIndex = $scope.getMonths.selectedOption.value;
+		var _year = $scope.getYears.selectedOption;		
+		var _firstDayofCalendar = new Date(_year, _monthIndex, 1);
+		
+		//What date is the first sunday of the month?	
+		if(_firstDayofCalendar.getDay() > 0)
+			_firstDayofCalendar.setDate(1 - _firstDayofCalendar.getDay());
+		else
+			_firstDayofCalendar.setDate(-6);		
+			
+		for(var _i=0; _i < (7*6); _i++){
+			if( _monthIndex === _firstDayofCalendar.addDays(_i).getMonth() )
+				if( new Date().toDateString() === _firstDayofCalendar.addDays(_i).toDateString() )
+					_calendarData.push({id : _i, day : _firstDayofCalendar.addDays(_i).getDate(), isToday : true, thisMonth : true});
+				else
+					_calendarData.push({id : _i, day : _firstDayofCalendar.addDays(_i).getDate(), isToday : false, thisMonth : true})
+			else
+				_calendarData.push({id : _i, day : _firstDayofCalendar.addDays(_i).getDate(), isToday : false, thisMonth : false});
+		} 
+		
+		return _calendarData;	
+	}	// End function GetMonthData
+	
+	function MoveMonthForward(){
+		if($scope.getMonths.selectedOption.value + 1 == 12){
+			if($scope.getYears.selectedOption + 1 > UPPERYEARLIMIT){
+				// Disable forward cheveron		
+			} else {
+				$scope.getMonths.selectedOption = $scope.getMonths.availableOptions[0];
+				$scope.getYears.selectedOption = $scope.getYears.selectedOption + 1;
+			}
+		} else			
+			$scope.getMonths.selectedOption = $scope.getMonths.availableOptions[$scope.getMonths.selectedOption.value + 1];		
+	}	// End function MoveMonthForward
+	
+	function MoveWeekForward(){
+		if ($scope.getWeeks.selectedOption.value + 1 == 6){
+			if($scope.getMonths.selectedOption.value + 1 == 12){
+				if($scope.getYears.selectedOption + 1 > UPPERYEARLIMIT){
+					// Disable forward cheveron	
+				} else {
+					$scope.getWeeks.selectedOption = $scope.getWeeks.availableOptions[0];
+					$scope.getMonths.selectedOption = $scope.getMonths.availableOptions[0];
+					$scope.getYears.selectedOption = $scope.getYears.selectedOption + 1;
+				}					
+			} else {
+				$scope.getWeeks.selectedOption = $scope.getWeeks.availableOptions[0];
+				$scope.getMonths.selectedOption = $scope.getMonths.availableOptions[$scope.getMonths.selectedOption.value + 1];
+			}	
+		} else
+			$scope.getWeeks.selectedOption = $scope.getWeeks.availableOptions[$scope.getWeeks.selectedOption.value];
+	}	// End function MoveWeekForward
+	
+	function MoveMonthBackward(){
+		if ($scope.getMonths.selectedOption.value - 1 == -1){
+			if ($scope.getYears.selectedOption - 1 < LOWERYEARLIMIT){
+				// Disable forward cheveron		
+			} else {
+				$scope.getMonths.selectedOption = $scope.getMonths.availableOptions[11];
+				$scope.getYears.selectedOption = $scope.getYears.selectedOption - 1;
+			}
+		} else			
+			$scope.getMonths.selectedOption = $scope.getMonths.availableOptions[$scope.getMonths.selectedOption.value - 1];
+	}	// End function MoveMonthBackward
+	
+	function MoveWeekBackward(){
+		if ($scope.getWeeks.selectedOption.value - 1 == 0){
+			if($scope.getMonths.selectedOption.value - 1 == -1){
+				if(($scope.getYears.selectedOption - 1) < LOWERYEARLIMIT){
+					// Disable forward cheveron	
+				} else {
+					$scope.getWeeks.selectedOption = $scope.getWeeks.availableOptions[4];
+					$scope.getMonths.selectedOption = $scope.getMonths.availableOptions[11];
+					$scope.getYears.selectedOption = $scope.getYears.selectedOption - 1;
+				}					
+			} else {
+				$scope.getWeeks.selectedOption = $scope.getWeeks.availableOptions[4];
+				$scope.getMonths.selectedOption = $scope.getMonths.availableOptions[$scope.getMonths.selectedOption.value - 1];
+			}	
+		} else
+			$scope.getWeeks.selectedOption = $scope.getWeeks.availableOptions[$scope.getWeeks.selectedOption.value - 2];
+	}	// End function MoveWeekBackward
+	
+	// Initialize scope variables
+	$scope.ShowView = function(viewName){
+		switch(viewName){
+			case "week":
+				$scope.getViews.selectedOption = LoadViews().availableOptions[1];
+				break;
+			case "day":
+				$scope.getViews.selectedOption = LoadViews().availableOptions[2];
+				break;
+			default:
+				$scope.getViews.selectedOption = LoadViews().availableOptions[0];
+				break;			
+		}	// End switch
+		
+		$scope.GetCalendarData();
+	}	// End function ShowMonthView
+	
+	$scope.GetCalendarData = function(){			
+		switch($scope.getViews.selectedOption){
+			case LoadViews().availableOptions[1]:
+				$scope.calendarData = GetWeekData();
+				break;
+			default:
+				$scope.calendarData = GetMonthData();
+				break;			
+		}	// End switch
+	}	// End function GetCalendarDataData	
+	
+	$scope.MoveCalendarForward = function(){
+		if($scope.getViews.selectedOption == "week")
+			MoveWeekForward();
+		else
+			MoveMonthForward();
+		
+		$scope.GetCalendarData();
+	}	// End function MoveCalendarForward
+	
+	$scope.MoveCalendarBackward = function(){
+		if($scope.getViews.selectedOption == "week")
+			MoveWeekBackward();
+		else
+			MoveMonthBackward();
+		
+		$scope.GetCalendarData();
+	}	// End function MoveCalendarBackward
+	
+	// Initialize scope variables	
+	$scope.getViews = LoadViews();
+	$scope.getMonths = LoadMonths();
+	$scope.getYears = LoadYears();		
+	$scope.getWeeks = LoadWeeks();
+	$scope.getDays = LoadDays($scope.getYears.selectedOption, $scope.getMonths.selectedOption.value);
+	$scope.GetCalendarData();
 });
 
 /***
@@ -217,7 +390,7 @@ app.controller('ResetCtrl', function($scope, $location, locationHandler, userSes
 /***
 *	Login Controller
 ***/
-app.controller('LoginCtrl', function($scope, locationHandler, $location, userSession) {
+app.controller('LoginCtrl', function($scope, locationHandler, $location, $http, userSession) {
 	
 	// Alert the user that their password was successfully reset if returning from reset password page
 	var message = document.getElementById("message");
@@ -280,7 +453,7 @@ app.controller('MainCtrl', function($scope, $location, locationHandler) {
 	}
 	
 	$scope.IsActive = function(viewName){
-		return (viewName == '' && $location.path().substring(1) != 'admin' ) ? true : ( $location.path().substring(1) == viewName ) ? true : false;
+		return (viewName == 'dashboard' && $location.path().substring(1) != 'admin' ) ? true : ( $location.path().substring(1) == viewName ) ? true : false;
 	}
 });
 
